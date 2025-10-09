06025adb0c37c46613d9b335bc425be656d83577
Pete Ahearn <Peter_Ahearn@cable.comcast.com>
2025-10-09 09:36:28 -0400

    AVInput COM-RPC Support: Debugging unit tests

--- Tests/L1Tests/tests/test_AVInput.cpp
+++ Tests/L1Tests/tests/test_AVInput.cpp
@@ -54,9 +54,7 @@ protected:
     AVInputMock* p_avInputMock = nullptr;
     DECL_CORE_JSONRPC_CONX connection;
     IarmBusImplMock* p_iarmBusImplMock = nullptr;
-    // <pca> debug
-    //ManagerImplMock* p_managerImplMock = nullptr;
-    // </pca>
+    ManagerImplMock* p_managerImplMock = nullptr;
     PLUGINHOST_DISPATCHER* dispatcher;
 
     AVInputTest()
@@ -69,6 +67,13 @@ protected:
         , workerPool(Core::ProxyType<WorkerPoolImplementation>::Create(
           2, Core::Thread::DefaultStackSize(), 16))
     {
+        p_managerImplMock  = new NiceMock <ManagerImplMock>;
+        device::Manager::setImpl(p_managerImplMock);
+
+        EXPECT_CALL(*p_managerImplMock, Initialize())
+            .Times(::testing::AnyNumber())
+            .WillRepeatedly(::testing::Return());
+            
         p_avInputMock  = new NiceMock<AVInputMock>;
 
         dispatcher = static_cast<PLUGINHOST_DISPATCHER*>(
@@ -89,15 +94,6 @@ protected:
 
         p_iarmBusImplMock  = new NiceMock <IarmBusImplMock>;
         IarmBus::setImpl(p_iarmBusImplMock);
-
-        // <pca> debug
-        // p_managerImplMock  = new NiceMock <ManagerImplMock>;
-        // device::Manager::setImpl(p_managerImplMock);
-
-        // EXPECT_CALL(*p_managerImplMock, Initialize())
-        //     .Times(::testing::AnyNumber())
-        //     .WillRepeatedly(::testing::Return());
-        // </pca>// <pca> debug
 
         Core::IWorkerPool::Assign(&(*workerPool));
         workerPool->Run();
@@ -126,14 +122,12 @@ protected:
             p_avInputMock = nullptr;
         }
 
-        // <pca> debug
-        // device::Manager::setImpl(nullptr);
-        // if (p_managerImplMock != nullptr)
-        // {
-        //     delete p_managerImplMock;
-        //     p_managerImplMock = nullptr;
-        // }
-        // </pca>
+        device::Manager::setImpl(nullptr);
+        if (p_managerImplMock != nullptr)
+        {
+            delete p_managerImplMock;
+            p_managerImplMock = nullptr;
+        }
     }
 };
 
