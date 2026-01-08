bf86422ea0ea5c3770d054cbdc2c7a3adb7c7766
balav08 <54432605+balav08@users.noreply.github.com>
2026-01-08 11:12:13 +0530

    Remove const reference from function parameters

--- AVOutput/AVOutputTVHelper.cpp
+++ AVOutput/AVOutputTVHelper.cpp
@@ -1336,7 +1336,7 @@ namespace Plugin {
          return "UNKNOWN ERROR";
     }
 
-    int AVOutputTV::getSaveConfig(const std::string& param,const capDetails_t& capInfo, valueVectors_t &values)
+    int AVOutputTV::getSaveConfig(const std::string& param, capDetails_t& capInfo, valueVectors_t &values)
     {
         LOGINFO("Entry : %s pqmode : %s source :%s format :%s component : %s color : %s control:%s\n",__FUNCTION__,capInfo.pqmode.c_str(),capInfo.source.c_str(),capInfo.format.c_str(),capInfo.component.c_str(),capInfo.color.c_str(),capInfo.control.c_str());
 
@@ -2287,7 +2287,7 @@ namespace Plugin {
         return 0;
     }
 
-    int AVOutputTV::ReadCapablitiesFromConf(const std::string& param, capDetails_t& info)
+    int AVOutputTV::ReadCapablitiesFromConf(std::string param, capDetails_t& info)
     {
         int ret = 0;
 
