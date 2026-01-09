8bd70a0ac5412d14d83521c41091145277c907c6
smanes0213 <sankalpmaneshwar46@outlook.com>
2026-01-09 14:10:40 +0530

    test_HdmiCecSource.cpp

--- Tests/L1Tests/tests/test_HdmiCecSource.cpp
+++ Tests/L1Tests/tests/test_HdmiCecSource.cpp
@@ -1858,8 +1858,8 @@ TEST_F(HdmiCecSourceInitializedEventTest, giveDeviceVendorIdProcess_LGTV){
 
     EXPECT_NO_THROW(Plugin::HdmiCecSourceImplementation::_instance->OnDisplayHDMIHotPlug(dsDISPLAY_EVENT_CONNECTED));
 
-    // Give the detached thread time to complete
-    usleep(100 * 1000); // sleep for 100 milliseconds
+    // Give the detached thread time to complete to avoid race condition with test cleanup
+    usleep(500 * 1000); // sleep for 500 milliseconds
 
     EVENT_UNSUBSCRIBE(0, _T("onHdmiHotPlug"), _T("client.events.onHdmiHotPlug"), message);
 
